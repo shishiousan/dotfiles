3889d1ce90768e33eb10d7373a6b373b477531b4
shion <106575883+shishiousan@users.noreply.github.com>
2024-02-17 17:55:02 +0900

    Updates dotfiles
    - adding nushell config
    - adding oh-my-posh themes

---
+++ config/nvim/lua/plugins/nu.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "nushell/tree-sitter-nu" },
+    },
+    build = ":TSUpdate",
+  },
+  {
+    "LhKipp/nvim-nu",
+    filetypes = { "nu" },
+  },
+}
