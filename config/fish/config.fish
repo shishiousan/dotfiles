b02c41c063da982c23b678ae3f24fa76ffa0c5ed
shion <106575883+shishiousan@users.noreply.github.com>
2024-02-18 17:07:34 +0900

    Updates dotfiles
    - adding vi mode setting for fish shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -124,6 +124,32 @@ set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH $HOME/.local/lib
 # set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/lib/x86_64-linux-gnu
 # set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/lib/python3.10/config-3.10-x86_64-linux-gnu
 
+
 # oh-my-posh init fish | source
 # oh-my-posh init fish --config $HOME/.config/omp/themes/catppuccin.omp.json | source
 oh-my-posh init fish --config $HOME/.config/omp/themes/velvet.omp.json | source
+
+# activate vim mode without erasing any default key bindings 
+function my_vi_mode
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+
+    fish_vi_key_bindings --no-erase
+
+    bind -M insert -m default jj cancel repaint-mode
+
+    # Due to the cursor_default, it is redundant 
+    # set fish_cursor_visual block
+end
+
+set fish_cursor_default block
+set fish_cursor_insert line blink
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+# Set the external cursor to a line. The external cursor appears when a command is started.
+set fish_cursor_external line blink
+set fish_vi_force_cursor 0
+
+my_vi_mode
+fish_vi_cursor
