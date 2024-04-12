8cd3101a18a9806ad756b94fdc14c8eae2297641
shion <shimizu.shion819@gmail.com>
2024-04-12 18:44:53 +0900

    Updates in dotfiles
    - adding copilot nvim plugin

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.keymap.set("i", "<C-M>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+
+      -- to cooperate with nvim-cmp
+      vim.g.copilot_no_tab_map = true
+
+      -- TODO: translate this into lua ver
+      vim.cmd([[ 
+      let g:copilot_filetypes = {
+          \ '*': v:false, 
+          \ 'fortran': v:true, 
+          \ 'lua': v:true, 
+          \ 'go': v:true, 
+          \}
+      ]])
+
+      -- vim.g.copilot_filetypes = {
+      --   fortran = false,
+      -- }
+      -- vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
+      --vim.keymap.set("i", "<M-/>", "<Plug>(copilot-suggest)")
+    end,
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          local stat = vim.g.copilot_enabled
+          if stat == nil or stat == 1 then
+            vim.cmd([[Copilot disable]])
+            vim.notify("Copilot disabled")
+          elseif stat == 0 then
+            vim.cmd([[Copilot enable]])
+            vim.notify("Copilot enabled")
+          else
+            vim.notify("Failed to toggle Copilot", 4)
+          end
+        end,
+        mode = { "n" },
+        desc = "Toggle copilot",
+        -- NOTE: this key overwrite default key for codelens
+      },
+      {
+        "<C-S-CR>",
+        function()
+          vim.cmd([[Copilot panel]])
+        end,
+        mode = { "i" },
+        desc = "Open copilot panel",
+      },
+    },
+  },
+}
