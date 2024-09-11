df35378a4ccc330b1a5afdc651df470bae466d8d
shion <shimizu.shion819@gmail.com>
2024-09-11 10:53:31 +0900

    Updating dotfiles
    - fixing the issue related which-key

--- config/nvim/lua/plugins/latex.lua
+++ config/nvim/lua/plugins/latex.lua
@@ -26,63 +26,61 @@ return {
         pattern = { "bib", "tex" },
         callback = function()
           local wk = require("which-key")
-          wk.register({
-            v = {
-              name = "+vimtex",
-              a = { "<cmd>Telescope bibtex<CR>", "add citation (from bib)" },
-              b = { "<cmd>VimtexCompile<CR>", "build" },
-              c = { "<cmd>VimtexCountWords!<CR>", "count" },
-              i = { "<cmd>VimtexTocOpen<CR>", "index" },
-              v = { "<cmd>VimtexView<CR>", "view" },
-              d = { "<cmd>VimtexClean<CR>", "delete aux" },
-              l = { "<cmd>VimtexErrors<CR>", "log of errors" },
-              m = { "<plug>(vimtex-context-menu)", "vimtex menu" },
-              r = { "<plug>(vimtex-doc-package)", "read vimtex doc" },
-              e = {
-                function()
-                  -- check vimtex auxiliary direcotry variable
-                  local out_dir = os.getenv("VIMTEX_OUTPUT_DIRECTORY")
-                  if type(out_dir) == "string" then
-                    local do_exist, _ = os.rename(out_dir, out_dir)
-                    if do_exist == true then
-                      vim.cmd("! bibexport -o %:r.bib ./" .. out_dir .. "/%:r.aux")
-                      return
-                    end
+          wk.add({
+            { "<leader>v", group = "VIMTEX" },
+            { "<leader>va", "<cmd>Telescope bibtex<CR>", desc = "Add CITATION (bib)" },
+            { "<leader>vb", "<cmd>VimtexCompile<CR>", desc = "BUILD" },
+            { "<leader>vc", "<cmd>VimtexCountWords!<CR>", desc = "COUNT" },
+            { "<leader>vd", "<cmd>VimtexClean<CR>", desc = "DELETE aux" },
+            {
+              "<leader>ve",
+              function()
+                -- check vimtex auxiliary direcotry variable
+                local out_dir = os.getenv("VIMTEX_OUTPUT_DIRECTORY")
+                if type(out_dir) == "string" then
+                  local do_exist, _ = os.rename(out_dir, out_dir)
+                  if do_exist == true then
+                    vim.cmd("! bibexport -o %:r.bib ./" .. out_dir .. "/%:r.aux")
+                    return
                   end
-                  vim.cmd("! bibexport -o %:r.bib %:r.aux")
-                end,
-                "Make bib file",
-              },
-              f = { -- Indentation format by latexindent.pl
-                function()
-                  vim.cmd("normal! w") -- before format, make sure that the file is latest
-                  if not vim.bo.filetype == "tex" then
-                    vim.notify("Only default save was done (no format)", 3, { title = "This is not TeX file" })
-                  end
-                  local out_dir = os.getenv("VIMTEX_OUTPUT_DIRECTORY")
-                  if type(out_dir) == "string" then
-                    local do_exist, _ = os.rename(out_dir, out_dir)
-                    if do_exist then
-                      -- NOTE: -c option determine the location of indent.log file
-                      -- -o specifies the outputfile without backup
-                      -- if you want the backup file use -w or -ww option
-                      vim.cmd("! latexindent -c " .. out_dir .. " -s -o % %")
-                      return
-                    end
-                  end
-                  vim.cmd("! latexindent -s -o % %")
-                end,
-                "format save (no backup)",
-              },
+                end
+                vim.cmd("! bibexport -o %:r.bib %:r.aux")
+              end,
+              desc = "MAKE bib file",
             },
-          }, { prefix = "<leader>", mode = { "n" } })
+            {
+              "<leader>vf",
+              function()
+                vim.cmd("normal! w") -- before format, make sure that the file is latest
+                if not vim.bo.filetype == "tex" then
+                  vim.notify("Only default save was done (no format)", 3, { title = "This is not TeX file" })
+                end
+                local out_dir = os.getenv("VIMTEX_OUTPUT_DIRECTORY")
+                if type(out_dir) == "string" then
+                  local do_exist, _ = os.rename(out_dir, out_dir)
+                  if do_exist then
+                    -- NOTE: -c option determine the location of indent.log file
+                    -- -o specifies the outputfile without backup
+                    -- if you want the backup file use -w or -ww option
+                    vim.cmd("! latexindent -c " .. out_dir .. " -s -o % %")
+                    return
+                  end
+                end
+                vim.cmd("! latexindent -s -o % %")
+              end,
+              desc = "FORMAT SAVE (no backup)",
+            },
+            { "<leader>vi", "<cmd>VimtexTocOpen<CR>", desc = "INDEX" },
+            { "<leader>vl", "<cmd>VimtexErrors<CR>", desc = "LOG of ERRORS" },
+            { "<leader>vm", "<plug>(vimtex-context-menu)", desc = "VIMTEX MENU" },
+            { "<leader>vr", "<plug>(vimtex-doc-package)", desc = "READ VIMTEX DOC" },
+            { "<leader>vv", "<cmd>VimtexView<CR>", desc = "VIEW" },
+          })
           vim.wo.conceallevel = 2
         end,
       })
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-      -- PDF Viewer:
-      -- http://manpages.ubuntu.com/manpages/trusty/man5/zathurarc.5.html
       vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_quickfix_mode = 0
 
